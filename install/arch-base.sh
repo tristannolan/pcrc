f9ec45170be5c9d62e6dec65816cef279636a098
Tristan Nolan <tristannolan@users.noreply.github.com>
2026-02-04 11:34:29 +0200

    arch edit

--- install/arch-base.sh
+++ install/arch-base.sh
@@ -2,16 +2,17 @@
 
 # Usage
 # Create a bootable usb drive with an arch iso
-# Boot arch
+# Boot the vm/pc
 
-# Download the file
-# curl -sL [URL] -o $SCRIPT.sh
-
-# Edit to configure settings
-# sudo $SCRIPT.sh
+# curl https://raw.githubusercontent.com/tristannolan/dotfiles/refs/heads/main/install/arch-base.sh -o arch-base.sh
+# vim arch-base.sh
+# chmod +x arch-base.sh
+# ./arch-base.sh
 
 # Settings
 safe_to_run=false
+
+keyboard_layout=us
 
 # Arguments
 case "$1" in
@@ -23,12 +24,12 @@ case "$1" in
 	;;
 esac
 
-# Script
-#read -p "Prompting a question: " answer
-#echo $answer
-
+# Safety net
 if [ "$safe_to_run" != "true" ] && [ "$unsafe" != "true" ]; then
 	echo "Aborting - Not safe to run"
 	echo "Please review and modify settings before continuing"
 	exit 1
 fi
+
+# Keyboard layout
+echo "loadkeys $keyboard_layout"
